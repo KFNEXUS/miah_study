aaebadd1e3862055a2d4dc81b40d82d14c25d5e0
kfnexus <ageofkings@ya.ru>
2020-11-04 18:08:14 +0300

    Алекандр заставил спомнить школу.

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-print(123)
-new = input('введите число')
-print(new*20)
+import random
+import math
+# print(math.sqrt(64))
+print('ax^2+bx+c')
+a = int(input("введите a "))
+b = int(input("введите b "))
+c = int(input("введите c "))
+D = (b**2)-(4*a*c)
+kD = math.sqrt(D)
+x1 = (-b+kD)/(2*a)
+x2 = (-b-kD)/(2*a)
+print('x1 = ' + str(x1) + ' x2 = ' + str(x2))
+
+
+
+"""
+Первая программа на питоне
+
+
+print("fgdfgd"+" fdgdf" +" fdgdf" )
+print((2**5)+(32+10))
+
+print('hello word')
+balistic_skill = 111
+
+
+input_number = int(input("введите число"))
+print('число: ', "(" + str(input_number) + ")")
+print(type(input_number))
+# получим д6
+print('случайное число', random.randint(1, 6))
+"""
