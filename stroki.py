0e4946dd79877b1548758bd56e3e39b8b7f27d78
kfnexus <ageofkings@ya.ru>
2020-11-12 23:19:54 +0300

    Функции, строки итп

---
+++ stroki.py
@@ -0,0 +1,25 @@
+c = 'k435ljj'
+
+print(c.find('j'))
+c = c[:4] + 'X' + c[4:]
+print(c)
+print(c[0:5:2])
+print(c[:-5])
+print(c[5:])
+
+for elem in c:
+    print(elem)
+
+
+#Perivii_siimvolъ
+print(c[0])
+
+#длина строки
+print(len(c))
+print(c[len(c)-1])
+print(c[-2])
+print(c.__len__())
+str(123)
+
+x = 'ads"d\n123\\1'\
+    'dfgdfg'
